2f78f4d8dac768989828700b1f7982e63b619c31
Davey Seeman <41802174+DaveySeeman@users.noreply.github.com>
2020-03-27 20:47:12 -0400

    Add files via upload
    The website works when all of these files are in the same folder.

---
+++ disasterfunctions.js
@@ -0,0 +1,13 @@
+//Feedback and Souces Button Popup Functions
+
+    // When the user clicks on <div>, open the popup
+    function feedback() {
+      var popup = document.getElementById("myPopup");
+      popup.classList.toggle("show");
+    }
+
+    // When the user clicks on <div>, open the popup
+    function sources() {
+      var popup2 = document.getElementById("myPopup2");
+      popup2.classList.toggle("show");
+    }
